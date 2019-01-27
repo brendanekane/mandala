825a485f6763a1f90e354478e8b2becdb1743b74
Brendan <brendanekane@gmail.com>
2019-01-27 15:27:07 -0500

    trying dynamic resize of canvas

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 const DrawingBoard = require('./drawing_board.js');
+// trying to have canvas resize properly but cant do it with viewport units
+//
+// const resize = () => {
+//   const canvas = document.querySelector("#mandala-canvas"),
+//   height = window.innerHeight,
+//   ratio = canvas.width/canvas.height,
+//   width = height * ratio;
+//
+//   canvas.style.width = '50vw';
+//   canvas.style.height = '100vh';
+// };
 
 document.addEventListener("DOMContentLoaded", () => {
   const canvas = document.querySelector("#mandala-canvas");
   canvas.height = 700;
   canvas.width = 700;
   canvas.style.border = "solid black";
+
+
   const ctx = canvas.getContext("2d");
   const board = new DrawingBoard(ctx);
   board.init();
 });
+
+// window.addEventListener('load', resize, false);
+// window.addEventListener('resize', resize, false);
